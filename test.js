5a1abcf719aa3b42c681cd921ad3d4d089ad9a2f
Yessine Mimita <yessinemimita@takiacademyteam.com>
2023-12-08 14:18:22 +0000

    :hammer: test:palindrome test with word mom

--- test.js
+++ test.js
@@ -3,3 +3,7 @@ const { isPalindrome } = require("./palindrome");
 test("Empty input", () => {
   expect(isPalindrome("")).toBe("The input is empty");
 });
+
+test("Palindrome input", () => {
+  expect(isPalindrome("mom")).toBe("The input is a palindrome");
+});
