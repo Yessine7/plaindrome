48412ce3133e7b4ed47473fc0cf220f317661809
Yessine Mimita <yessinemimita@takiacademyteam.com>
2023-12-08 14:17:35 +0000

    :hammer: logic:added palindrome function logic

--- palindrome.js
+++ palindrome.js
@@ -2,6 +2,14 @@ function isPalindrome(input) {
   if (!input) {
     return "The input is empty";
   }
+  const cleanedInput = input.toLowerCase().replace(/[^a-z]/g, "");
+  const reversedInput = cleanedInput.split("").reverse().join("");
+
+  if (cleanedInput === reversedInput) {
+    return "The input is a palindrome";
+  } else {
+    return "The input is not a palindrome";
+  }
 }
 
 module.exports = { isPalindrome };
